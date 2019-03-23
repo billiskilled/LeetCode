bc304093aa9b11d00f0fe021b93e797de811b53b
wanglei <wanglei@iyunxiao.com>
2019-03-23 17:05:45 +0800

    java解决链表反转

---
+++ Cn/Java/206/S_H.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ */
+
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        System.out.println("you are ok");
+        System.out.println("javacomplete is not ok");
+    }
+}
+
+pulic class S_H {
+
+}
