e5e168b7911b5d36662872696d1cb840150e7cc1
wanglei <wanglei@iyunxiao.com>
2019-05-04 19:50:50 +0800

    添加了第20题的解法

--- Cn/Go/20.有效的括号.go
+++ Cn/Go/20.有效的括号.go
@@ -12,47 +12,86 @@
  * Testcase Example:  '"()"'
  *
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- * 
+ *
  * 有效字符串需满足：
- * 
- * 
+ *
+ *
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合。
- * 
- * 
+ *
+ *
  * 注意空字符串可被认为是有效字符串。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: "()"
  * 输出: true
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: "()[]{}"
  * 输出: true
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
+ *
  * 输入: "(]"
  * 输出: false
- * 
- * 
+ *
+ *
  * 示例 4:
- * 
+ *
  * 输入: "([)]"
  * 输出: false
- * 
- * 
+ *
+ *
  * 示例 5:
- * 
+ *
  * 输入: "{[]}"
  * 输出: true
- * 
+ *
  */
-func isValid(s string) bool {
-    
-}
+// package main
 
+// import "fmt"
+
+// func main() {
+// 	s := "()"
+// 	result := isValid(s)
+// 	fmt.Print("Result: ", result)
+// }
+
+func isValid(s string) bool {
+	var byteArr []byte
+	byteArr = []byte(s)
+
+	index := 0
+	var stack []byte
+	stack = make([]byte, len(s))
+
+	for _, e := range byteArr {
+		if e == byte('{') || e == byte('(') || e == byte('[') {
+			stack[index] = e
+			index++
+		} else {
+			if e == byte('}') && index > 0 && stack[index-1] == byte('{') {
+				index--
+				continue
+			}
+			if e == byte(']') && index > 0 && stack[index-1] == byte('[') {
+				index--
+				continue
+			}
+			if e == byte(')') && index > 0 && stack[index-1] == byte('(') {
+				index--
+				continue
+			}
+			return false
+		}
+	}
+	if index == 0 {
+		return true
+	}
+	return false
+}
